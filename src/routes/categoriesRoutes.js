b984e608efaa928b79391decdf809c303014fe5d
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-05-30 00:42:56 -0300

    fix: ajuste nos imports

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { listCategories, insertCategory } from './../controllers/categoriesController.js';
-import { validateCategoy } from './../middlewares/categoreisValidations.js';
+import { validateCategoy } from './../middlewares/categoriesValidations.js';
 
 const router = express.Router();
 router.get("/categories", listCategories);
